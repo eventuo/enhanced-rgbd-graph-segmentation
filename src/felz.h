89fcb84c8bf85f5d83a8b042d1ed2035185dbb5b
eventuo <eventuoto@proton.me>
2021-11-09 20:33:55 +0000

    tracepoint: Do not waste memory on mods with no tracepoints

--- src/felz.h
+++ src/felz.h
@@ -2,4 +2,6 @@
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
  *
- * Redistribution and use in
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
