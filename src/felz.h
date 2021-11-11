d0b76a9f93de582410c1aacc9178598fb70a90d1
eventuo <eventuoto@proton.me>
2021-11-11 13:36:54 +0000

    fullscreen

--- src/felz.h
+++ src/felz.h
@@ -10,4 +10,6 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the na
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written
