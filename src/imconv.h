ec2914e88e974cbbfba840f739aa68d427ae12d1
eventuo <eventuoto@proton.me>
2022-08-08 15:42:08 +0000

    ALSA: pcm: Fix snd_pcm_hw_params struct copy in compat mode

--- src/imconv.h
+++ src/imconv.h
@@ -139,4 +139,11 @@ static image<uchar> *imageLONGtoUCHAR(image<long> *input, long min, long max) {
   float scale = UCHAR_MAX / (float)(max - min);
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      uchar val = (uchar)((i
+      uchar val = (uchar)((imRef(input, x, y) - min) * scale);
+      imRef(output, x, y) = bound(val, (uchar)0, (uchar)UCHAR_MAX);
+    }
+  }
+  return output;
+}
+
+static image<uchar> *
