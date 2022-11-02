64c8ec793dd612c2672f0ddd843eae4461581fac
eventuo <eventuoto@proton.me>
2022-11-02 16:09:52 +0000

    Update Request.php

--- src/imutil.h
+++ src/imutil.h
@@ -25,4 +25,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include "misc.h"
 namespace felzenszwalb {
 
-/* compute minimum and maximu
+/* compute minimum and maximum value in an image */
+template <class T>
+void min_max(image<T> *im, T *ret_min, T *ret_max) {
+  int width = im->width();
+  int height
