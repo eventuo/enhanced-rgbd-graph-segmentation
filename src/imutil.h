349051c72b1363940d69ed8a52da63bc9dfd2bdc
eventuo <eventuoto@proton.me>
2022-12-23 09:52:45 +0000

    1.1.21

--- src/imutil.h
+++ src/imutil.h
@@ -55,4 +55,14 @@ image<uchar> *threshold(image<T> *src, int t) {
   image<uchar> *dst = new image<uchar>(width, height);
   
   for (int y = 0; y < height; y++) {
-    for (int x = 0; x < wi
+    for (int x = 0; x < width; x++) {
+      imRef(dst, x, y) = (imRef(src, x, y) >= t);
+    }
+  }
+
+  return dst;
+}
+
+}
+#endif
+
