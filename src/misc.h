acd837a574f08e4bb49fc2ebade99256631e8bbc
eventuo <eventuoto@proton.me>
2023-03-20 08:39:45 +0000

    Updated the vtk feedstock.

--- src/misc.h
+++ src/misc.h
@@ -12,4 +12,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free So
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+*/
+
+/* random stuff */
+
+#ifndef MISC_H
+#define MISC_H
+
+#include <cmath>
+namespace 
