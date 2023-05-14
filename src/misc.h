27a2aa205b92c562d17d8925ea4a30f3e26603a2
eventuo <eventuoto@proton.me>
2023-05-14 08:35:48 +0000

    arm64: compat Fix siginfo_t -> compat_siginfo_t conversion on big endian

--- src/misc.h
+++ src/misc.h
@@ -60,4 +60,7 @@ inline int vlib_round(float x) { return (int)(x + 0.5F); }
 inline int vlib_round(double x) { return (int)(x + 0.5); }
 
 inline double gaussian(double val, double sigma) {
-  return ex
+  return exp(-square(val/sigma)/2)/(sqrt(2*M_PI)*sigma);
+}
+}
+#endif
