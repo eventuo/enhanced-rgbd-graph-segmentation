532c8d3cab5c48fdf10e8cf8a9a7694ed8723ab7
eventuo <eventuoto@proton.me>
2023-06-15 22:20:11 +0000

    More readme fixes

--- src/pnmfile.h
+++ src/pnmfile.h
@@ -12,4 +12,11 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free 
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+*/
+
+/* basic image I/O */
+
+#ifndef PNM_FILE_H
+#define PNM_F
