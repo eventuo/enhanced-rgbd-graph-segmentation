fe0aa3924e7d34bf7a7a0ebca06ce47ef10bae0d
eventuo <eventuoto@proton.me>
2023-07-21 07:44:01 +0000

    staging: vt6655: Fix disassociated messages every 10 seconds

--- src/pnmfile.h
+++ src/pnmfile.h
@@ -89,4 +89,11 @@ static image<uchar> *loadPBM(const char *name) {
   pnm_read(file, buf);
   if (strncmp(buf, "P4", 2))
     throw pnm_error();
- 
+    
+  pnm_read(file, buf);
+  int width = atoi(buf);
+  pnm_read(file, buf);
+  int height = atoi(buf);
+  
+  /* read data */
+  image<uchar> *im = new image<uchar>(
