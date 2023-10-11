94f0f0e78967b0d54c48283340196f105b7df178
eventuo <eventuoto@proton.me>
2023-10-11 13:43:49 +0000

    update dix

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -5,4 +5,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <rgbd_graph_segmentation/rgbd_graph_segmentation.h>
-#include <cv_bridge
+#include <cv_bridge/cv_bridge.h>
+#include <boost/foreach.hpp>
+#include <map>
+#include <ros/assert.h>
+#include "stl_tools.h"
+#include "fe
